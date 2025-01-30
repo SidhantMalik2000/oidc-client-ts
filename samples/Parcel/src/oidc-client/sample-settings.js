165b0064baad20f7b850629255cd6ece3c976c75
SidhantMalik2000 <sidhant.m@surya-soft.com>
2025-01-30 19:29:33 +0530

    settings changed to handle local auth server

--- samples/Parcel/src/oidc-client/sample-settings.js
+++ samples/Parcel/src/oidc-client/sample-settings.js
@@ -6,19 +6,16 @@ Log.setLevel(Log.INFO);
 const url = window.location.origin + "/oidc-client";
 
 export const settings = {
-    authority: "http://localhost:1234/oidc",
-    client_id: "js.tokenmanager",
+    authority: "https://localhost:2713",// my local auth server url
+    client_id: "d83d8922-5bd2-4b33-85ef-5d982320bf84",//default client id which is mapped in db for this client and the local redirect url
     redirect_uri: url + "/sample.html",
     post_logout_redirect_uri: url + "/sample.html",
     response_type: "code",
-    scope: "openid email roles",
+    scope: "openid email profile",
 
-    response_mode: "fragment",
+    response_mode: "form_post",
 
-    filterProtocolClaims: true
+    filterProtocolClaims: true,
 };
 
-export {
-    Log,
-    OidcClient
-};
+export { Log, OidcClient };
